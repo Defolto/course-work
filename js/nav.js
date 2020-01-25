9d0855ff073e0244e45163690a6d1f17e757f860
Defolto <37343435+Defolto@users.noreply.github.com>
2020-01-25 18:16:30 +0300

    add work with JSON and AJAX

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+const xmlhttp = new XMLHttpRequest();
+const nav = document.getElementById('subtypes');
+let type;
+
+// Слушатель клика по типам
+var element = document.querySelector("#types");
+element.addEventListener("click", function(e){
+    type = e.target.id;
+    xmlhttp.open("GET", "js/navList.json", true);
+    xmlhttp.send();
+});
+
+// AJAX
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let str;
+        var myObj = JSON.parse(this.responseText);
+        nav.innerHTML = '';
+        for (let i = 0; i < myObj.types[type].length; i++) {
+            str = myObj.types[type][i][0].toUpperCase() + myObj.types[type][i].slice(1)
+            nav.insertAdjacentHTML("beforeend", `<p class="row-nav__paragraph">${str}</p>`);
+        }
+    }
+};
+
